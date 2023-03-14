c79f959c8e90925b9e828b03f41a60b00406229a
moises gomez <moisesg526@yahoo.com>
2023-03-13 23:26:50 -0400

    Added sound function and keydown event listener to play sound when clicking buttons

--- game.js
+++ game.js
@@ -1,12 +1,16 @@
-const green = document.querySelector("#green-id");
-const red = document.querySelector("#red-id");
-const yellow = document.querySelector("#yellow-id");
-const blue = document.querySelector("#blue-id");
+const green = document.querySelector("#green");
+const red = document.querySelector("#red");
+const yellow = document.querySelector("#yellow");
+const blue = document.querySelector("#blue");
 
-green.addEventListener("click", playGreen)
-red.addEventListener("click", playRed)
-yellow.addEventListener("click", playYellow)
-blue.addEventListener("click", playBlue)
+// green.addEventListener("click", playGreen)
+green.addEventListener("click", nextSequence )
+// red.addEventListener("click", playRed)
+red.addEventListener("click", nextSequence )
+// yellow.addEventListener("click", playYellow)
+yellow.addEventListener("click", nextSequence )
+// blue.addEventListener("click", playBlue)
+blue.addEventListener("click", nextSequence )
 
 green.addEventListener("click", e => {
     console.log("Clicked on green");
@@ -22,27 +26,58 @@ blue.addEventListener("click", e => {
     console.log("Clicked on blue");
 })
 
-function playGreen() {
-    const audio = new Audio("/sounds/green.mp3");
-    audio.play();
-}
-function playRed() {
-    const audio = new Audio("/sounds/red.mp3");
-    audio.play();
-}
-function playYellow() {
-    const audio = new Audio("/sounds/yellow.mp3");
-    audio.play();
-}
-function playBlue() {
-    const audio = new Audio("/sounds/blue.mp3");
-    audio.play();
-}
 
-const colors = ["green", "red", "yellow", "blue"];
-const gameOrder = [];
-const playerOrder = [];
+// function playGreen() {
+//     const audio = new Audio("/sounds/green.mp3");
+//     audio.play();
+// }
+// function playRed() {
+//     const audio = new Audio("/sounds/red.mp3");
+//     audio.play();
+// }
+// function playYellow() {
+//     const audio = new Audio("/sounds/yellow.mp3");
+//     audio.play();
+// }
+// function playBlue() {
+//     const audio = new Audio("/sounds/blue.mp3");
+//     audio.play();
+// }
 
-const started = false;
+let colors = ["green", "red", "yellow", "blue"];
+let gameOrder = [];
+let playerOrder = [];
+let started = false;
+let level = 0;
+
+function nextSequence() {
+    playerOrder = [];
+
+    level++;
+    document.getElementById("instructions").innerHTML = `Level ${level}`;
+
+    let randomNum = Math.floor(Math.random() * 4);
+    let randomColor = colors[randomNum];
+    gameOrder.push(randomColor);
+    sounds(randomColor);
+};
 
 
+function sounds(name) {
+    let  audio = new Audio("sounds/" + name + ".mp3" );
+    audio.play();
+};
+
+document.addEventListener("keydown", function() {
+    if(!started) {
+        document.querySelector("#instructions").innerHTML = `Level ${level}`;
+        nextSequence();
+        started = true;
+    }
+});
+
+function startOver() {
+    level = 0;
+    gameOrder = [];
+    started = false;
+};
